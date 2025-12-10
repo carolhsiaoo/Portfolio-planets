50480970d79fdb93ac574f209d7c361c036af1ec
carolhsiaoo <zhuyuanxiao0@gmail.com>
2025-12-10 10:51:17 -0700

    Update Contact component with new social media links and change email address for improved contact information.

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,10 +1,11 @@
 export default function Contact() {
   const socialLinks = [
-    { name: "X", url: "#" },
-    { name: "INSTAGRAM", url: "#" },
-    { name: "LINKEDIN", url: "#" },
-    { name: "DRIBBBLE", url: "#" },
-    { name: "GITHUB", url: "#" },
+    { name: "X", url: "https://x.com/CarolXiaoo" },
+    { name: "INSTAGRAM", url: "https://www.instagram.com/itscarolstudio/?next=/" },
+    { name: "THREADS", url: "https://www.threads.net/@itscarolstudio" },
+    { name: "LINKEDIN", url: "https://www.linkedin.com/in/carol-hsiao-5779a1158/" },
+    { name: "DRIBBBLE", url: "https://dribbble.com/carolhsiao" },
+    { name: "GITHUB", url: "https://github.com/carolhsiaoo?tab=overview&from=2025-12-01&to=2025-12-05" },
   ];
 
   return (
@@ -13,10 +14,10 @@ export default function Contact() {
         <h2 className="text-xl sm:text-2xl font-serif mb-6 sm:mb-8 md:mb-10">Let's talk!</h2>
 
         <a
-          href="mailto:hello@carolhsiao.com"
+          href="mailto:carolhsiaostudio@gmail.com"
           className="text-base sm:text-lg font-normal hover:opacity-60 transition-opacity inline-block mb-6 sm:mb-8 md:mb-10"
         >
-          hello@carolhsiao.com
+          carolhsiaostudio@gmail.com
         </a>
 
         <div className="flex flex-wrap gap-4 sm:gap-6 md:gap-8 text-[10px] sm:text-xs font-medium tracking-wider">
