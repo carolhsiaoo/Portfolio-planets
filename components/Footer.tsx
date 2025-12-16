d727a316ca8a84e05c8415c5380a1d3b8f009233
carolhsiaoo <zhuyuanxiao0@gmail.com>
2025-12-16 13:48:23 -0700

    Update Footer component to align copyright text to the right for improved layout consistency.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = memo(function Footer() {
     <footer className="py-10 mt-20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 flex justify-between items-center text-xs sm:text-sm lg:text-base">
         <div>Made with nuts 🌰 by Carol Hsiao.</div>
-        <div>© 2025 All rights reserved</div>
+        <div className="text-right">© 2025 All rights reserved</div>
       </div>
     </footer>
   );
